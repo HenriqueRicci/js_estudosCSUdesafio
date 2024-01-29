ca4f6a319bde53271ed2396c8cee1f1ffc6d88e9
HenriqueRicci <henrique.ricci@live.com>
2024-01-29 16:17:30 -0300

    Retirando a variavel constante de URL, visto que a chamada para API é uma só durante o desafio

--- desafio.js
+++ desafio.js
@@ -1,7 +1,5 @@
-const url = "https://desafiotecnico314159265.free.beeceptor.com/"
-
 async function TransacaoAPI () {
-    const chamadaAPI = await fetch (url);
+    const chamadaAPI = await fetch ("https://desafiotecnico314159265.free.beeceptor.com/");
     console.log (chamadaAPI);
 
     const transacoes = await chamadaAPI.json();
