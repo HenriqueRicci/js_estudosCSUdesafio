489fb259bf5ed423a94460f5f8d0eaa70e379b9c
HenriqueRicci <henrique.ricci@live.com>
2024-02-05 12:18:50 -0300

    Deletando arquivos que não deu certo na minha tentativa de mockar dados da api

--- js/jest.config.mjs
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    testEnvironment: 'node', // O ambiente de teste (pode ser "jsdom" para testes no navegador)
-    moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node'], // Extensões de arquivo que Jest deve reconhecer
-    testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'], // Padrões de arquivo de teste que Jest deve procurar
-  };
